699e5f40ecc17387a3f0d03dabc1f6011e196c87
Bryan <bryan.curchod@gmail.com>
2018-11-09 18:26:58 +0100

    début

---
+++ gh-issues/src/components/topBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+class TopBar extends React.Component {
+    render(){
+            return (
+            <div id="topBar">
+                My very own GitHub Issue System
+            </div>
+        );
+    }
+}
+
+
+export default TopBar;
